824c2e2a1d06e0ba72761a8e6956dac720b462e8
ewanhughes26 <79140539+ewanhughes26@users.noreply.github.com>
2022-12-10 15:54:34 +0000

    Initial commit

---
+++ src/main/java/com/example/javafxsceneswitcher/SceneTwoController.java
@@ -0,0 +1,2 @@
+package com.example.javafxsceneswitcher;public class SceneTwoController {
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.javafxsceneswitcher {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.javafxsceneswitcher to javafx.fxml;
+    exports com.example.javafxsceneswitcher;
+}
